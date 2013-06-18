40d29daf9c6f58bd6eb81e71c3815e029cd5419e
bpsingh <bpsingh@pmtsl.onmicrosoft.com>
2013-06-18 14:44:11 +0530

    New changes

--- GitUseTest/AppDelegate.m
+++ GitUseTest/AppDelegate.m
@@ -15,10 +15,12 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    
+    // Override point for customization after application launch.
     self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
+    NSLog(@"application did finish launching");
+     NSLog(@"application did finish launching");
     return YES;
 }
 
